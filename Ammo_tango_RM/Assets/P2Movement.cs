55237ce2debbed74042165d3c1e11633f01ff314
SHOW-ME-TITTIES\Katava <eiole>
2019-04-15 11:43:50 +0300

    Shitty player cotnrols

---
+++ Ammo_tango_RM/Assets/P2Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Movement : MonoBehaviour
+{
+    protected Rigidbody MyRb;
+    public float MovementSpeed = 3f;
+
+    public void Start()
+    {
+        MyRb = GetComponent<Rigidbody>();
+    }
+    public void Update()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("LeftStickHorizontalP2"), 0, Input.GetAxis("LeftStickVerticalP2"));
+        Vector3 lookDirection = new Vector3(Input.GetAxis("RightStickHorizontalP2"), 0, Input.GetAxis("RightStickVerticalP2"));
+
+        transform.rotation = Quaternion.Euler(lookDirection);
+        MyRb.velocity = moveDirection * MovementSpeed;
+    }
+}
