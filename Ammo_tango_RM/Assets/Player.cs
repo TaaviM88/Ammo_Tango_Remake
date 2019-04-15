14ca265cf3ba5622fcc7a480a904bd6953780ad1
Ville W <ville.wessman@gmail.com>
2019-04-15 12:30:19 +0300

    Added: Weapon script, Player script with shoot -method, cooldown/reload, personal devscene.

---
+++ Ammo_tango_RM/Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour { 
+
+    public float HP;
+    public Weapon weapon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire3") && weapon.cooldownTimer() == true)
+        {
+            weapon.shoot();
+        }
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            transform.Rotate(0, 90, 0);
+        }
+    }
+}
---
+++ Ammo_tango_RM/Assets/Weapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour { 
+
+    public float damage;
+    public float cooldown;
+    private float timer = 0f;
+    public GameObject ammo;
+    public Player player;
+    public Transform shotspawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    public void shoot()
+    {
+        GameObject clone = Instantiate(ammo, shotspawn.transform.position, shotspawn.transform.rotation);
+
+        clone.GetComponent<Rigidbody>().velocity = new Vector3(transform.forward.x * 10,transform.forward.y * 10,transform.forward.z * 10);
+    }
+
+    public bool cooldownTimer()
+    {
+        
+        if(timer >= cooldown)
+        {
+            timer = 0f;
+            return true;
+        }
+        return false;
+    }
+}
