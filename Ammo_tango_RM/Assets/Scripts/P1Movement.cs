6da13405d34d49300efffa2657dc666d5636e2c8
Ville W <ville.wessman@gmail.com>
2019-04-16 11:54:44 +0300

    Removed duplicate P1Movement script.

--- Ammo_tango_RM/Assets/Scripts/P1Movement.cs
+++ Ammo_tango_RM/Assets/Scripts/P1Movement.cs
@@ -16,7 +16,7 @@ public class P1Movement : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("LeftStickHorizontalP1"), 0, Input.GetAxis("LeftStickVerticalP1"));
         Vector3 lookDirection = new Vector3(Input.GetAxis("RightStickHorizontalP1"), 0, Input.GetAxis("RightStickVerticalP1"));
 
-        transform.rotation = Quaternion.Euler(lookDirection);
         MyRb.velocity = moveDirection * MovementSpeed;
+        transform.rotation = Quaternion.LookRotation(lookDirection);
     }
 }
