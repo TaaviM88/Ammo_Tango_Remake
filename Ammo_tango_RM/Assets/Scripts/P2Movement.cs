dc966be9e2cb8d7b8ec68047781729db9920b6c4
SHOW-ME-TITTIES\Katava <eiole>
2019-04-17 12:29:02 +0300

    PlayerMovement, fixed rotation to not reset.

--- Ammo_tango_RM/Assets/Scripts/P2Movement.cs
+++ Ammo_tango_RM/Assets/Scripts/P2Movement.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class P2Movement : MonoBehaviour
 {
     protected Rigidbody MyRb;
-    public float MovementSpeed = 3f;
+    public float MovementSpeed = 10f;
+    private float maxVelocity = 10f;
+    private Vector3 lastRotation;
 
     public void Start()
     {
         MyRb = GetComponent<Rigidbody>();
     }
-    public void Update()
+    public void FixedUpdate()
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("LeftStickHorizontalP2"), 0, Input.GetAxis("LeftStickVerticalP2"));
         Vector3 lookDirection = new Vector3(Input.GetAxis("RightStickHorizontalP2"), 0, Input.GetAxis("RightStickVerticalP2"));
 
         MyRb.velocity = moveDirection * MovementSpeed;
-        transform.rotation = Quaternion.LookRotation(lookDirection);
+
+        if (MyRb.velocity.magnitude > maxVelocity)
+        {
+            MyRb.velocity = Vector3.ClampMagnitude(MyRb.velocity, maxVelocity);
+        }
+
+        if (lookDirection.sqrMagnitude > 0.1)
+        {
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+            lastRotation = lookDirection;
+        }
+        else
+        {
+            transform.rotation = Quaternion.LookRotation(lastRotation);
+        }
     }
 }
