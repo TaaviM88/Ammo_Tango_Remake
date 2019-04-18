eef05e425000536be4c2c5013b440f85fc8468c7
Akseli <azzeli91@gmail.com>
2019-04-18 11:54:10 +0300

    Added new scripts (MultipleTargetsCamera and PointofInterest)

---
+++ Ammo_tango_RM/Assets/Scripts/PointofInterest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointofInterest : MonoBehaviour
+{
+    private void Start()
+    {
+        MultipleTargetsCamera.Instance.AddTarget(transform);
+    }
+
+    private void OnDisable()
+    {
+        MultipleTargetsCamera.Instance.RemoveTarget(transform);
+    }
+}
