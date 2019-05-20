aaebf1e7d04e8026354c991fbd8967541a43a622
Remuzi <katava.sami@gmail.com>
2019-05-20 11:31:18 +0300

    removed old pplayerprefabs and fixed inverted inputs

--- Ammo_tango_RM/Assets/Scripts/Player.cs
+++ Ammo_tango_RM/Assets/Scripts/Player.cs
@@ -43,6 +43,7 @@ public class Player : MonoBehaviour {
     private GameObject shield;
     private GameObject weaponSlotObj;
     // Start is called before the first frame update
+
     void Start()
     {
         combat = gameObject.GetComponent<PlayerCombat>();
